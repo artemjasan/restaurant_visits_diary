86230031b5780a4452251f46d1d7763bfb64ecab
Artem Jasan <artjasan@gmail.com>
2022-05-23 21:59:45 +0200

    Add Swagger generator

--- restaurant_visits_project/core/settings.py
+++ restaurant_visits_project/core/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "dj_rest_auth",
     "dj_rest_auth.registration",
+    "drf_yasg",
 
     # Allauth required
     "django.contrib.sites",
--- restaurant_visits_project/core/urls.py
+++ restaurant_visits_project/core/urls.py
@@ -1,6 +1,21 @@
-
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Diary of visits to restaurants",
+        default_version="v1",
+        description="Assigment for ThinkEasy.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 auth_patterns = [
     path("", include("dj_rest_auth.urls")),
@@ -12,7 +27,17 @@ api_v1_patterns = [
     path("auth/", include([*auth_patterns]))
 ]
 
+swagger_patters = [
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+]
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include([*api_v1_patterns])),
+    path("api/", include(
+        [
+            *api_v1_patterns,
+            path("schema/", include(swagger_patters)),
+        ]
+    )),
 ]
