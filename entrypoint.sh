4af4a8c63ea8ac23c4b93b094b1ec63f21bf966e
Artem Jasan <artjasan@gmail.com>
2022-05-24 12:06:52 +0200

    Add Docker/Dcker-Compose, entrypoint, postgres settings

---
+++ entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -ex
+
+python restaurant_visits_project/manage.py runserver 0.0.0.0:8000
--- restaurant_visits_project/core/settings.py
+++ restaurant_visits_project/core/settings.py
@@ -94,9 +94,13 @@ REST_FRAMEWORK = {
 WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB", "diary"),
+        "USER": os.environ.get("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", 5432),
     }
 }
 
